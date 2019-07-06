f6ff6772f8a0dcc799e9958a15dcf03a3a428333
itmadmanuel <manueldavila.developer@gmail.com>
2019-07-06 11:04:47 -0500

    Nav animation

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        scroll = $(window).scrollTop()
+        if (scroll > 100) {
+            $('.menu').css({'position':'fixed'})
+            $('.menu').css({'width':'100%'})
+            $('.menu').css({'top':'0'})
+            $('.menu').css({'background':'#fff'})
+            $('.menu a').css({'color':'#000'})
+            $('.menu .logo').css({'color':'#000'})
+            $('.menu').css({'box-shadow':'rgba(0, 0, 0, 0.22) 6px 1px 1px'})
+            $('.menu').css({'z-index':'100'})
+        } else {
+            $('.menu').css({'position':'relative'})
+            $('.menu').css({'background':'transparent'})
+            $('.menu').css({'box-shadow':'0 0 0'})
+            $('.menu a').css({'color':'#fff'})
+            $('.menu .logo').css({'color':'#fff'})
+        }
+    })
+
+    $('.menu-icon').click(function() {
+        $('header nav').slideToggle()
+    })
+})
